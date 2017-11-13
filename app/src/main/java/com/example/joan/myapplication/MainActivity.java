507e2e80fdbc5973f936310486ad360a279a3da6
JoanShadow <joanpecamp1993@gmail.com>
2017-11-13 13:35:20 +0100

    activity's and layouts

--- app/src/main/java/com/example/joan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/joan/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.joan.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +15,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // Setear interfaz  R == carpeta res
+
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setText("Esta és la Activity 1");
+
+        Button buttonNewActivity = findViewById(R.id.buttonNewActivity);
+        buttonNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(MainActivity.class.getSimpleName(), "onClick");
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Log.d(MainActivity.class.getSimpleName(),"onCreate");
     }
